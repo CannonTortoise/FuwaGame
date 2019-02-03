31bff2c9a8ca40d26decd8ae608d47026588f4d0
CannonTortoise <ruiw2006@126.com>
2019-02-03 17:16:47 +0800

    Revert "Revert "Demo 0.02""
    This reverts commit f95940545371a0f0abcc2592485c5ef4e53ce3dc.

---
+++ Assets/Scripts/demo2/Death.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour {
+
+    public GameObject canvas;
+
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Ball") 
+            canvas.SetActive(true);
+    }
+    
+}
---
+++ Assets/Scripts/demo2/FollowController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//板子跟随鼠标移动
+public class FollowController : MonoBehaviour {
+
+    public Transform plank;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        plank.position = new Vector3(mousePos.x, mousePos.y, plank.position.z);
+    }
+}
