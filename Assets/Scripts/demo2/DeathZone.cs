24639b6df658129c9c49a8ef1b5737737fe871a5
tyf <812632647@qq.com>
2019-02-12 22:58:09 +0800

    接锅 我之前commit了错误的文件夹

---
+++ Assets/Scripts/demo2/DeathZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour {
+
+    public GameObject restartUI;
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("start");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.name == "Ball" )
+        {
+            
+            restartUI.SetActive(true);
+        }
+    }
+}
