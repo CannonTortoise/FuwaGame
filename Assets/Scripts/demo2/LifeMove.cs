27394c19ad2b18ea25b3f73260dc5c46167b5a3e
CannonTortoise <ruiw2006@126.com>
2019-02-21 16:38:32 +0800

    加入碰撞损坏 修改一些bug

--- Assets/Scripts/demo2/LifeMove.cs
+++ Assets/Scripts/demo2/LifeMove.cs
@@ -5,17 +5,30 @@ using UnityEngine.UI;
 
 public class LifeMove : MonoBehaviour {
     public Text lifeText;
+    private int HP;
+    public GameObject restartUI;
+
     public GameObject ball;
 	// Use this for initialization
 	void Start () {
+        HP = 100;
         lifeText.text = "100%";
         ball = GameObject.FindGameObjectWithTag("Player");
-
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (HP == 0)
+            restartUI.SetActive(true);
+        else if (HP <= 0)
+            HP = 0;
+        lifeText.text = HP + "%";
         lifeText.GetComponent<Transform>().position = ball.GetComponent<Transform>().position - new Vector3(0, 1, 0);
+        
 	}
+
+    public void ReduceHP(int n) {
+        HP -= n;
+    }
+
 }
