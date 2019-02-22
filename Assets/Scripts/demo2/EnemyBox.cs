4282eacfa04c4730d436f4f8656e8795a563ecff
oxx1108 <1551193484@qq.com>
2019-02-22 19:25:30 +0800

    修复了一个bug，添加了一个小关卡

--- Assets/Scripts/demo2/EnemyBox.cs
+++ Assets/Scripts/demo2/EnemyBox.cs
@@ -2,28 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinBox : MonoBehaviour {
+public class EnemyBox : MonoBehaviour
+{
 
-    public GameObject coin;
+    public GameObject enemy;
 
-	// Use this for initialization
-	void Start () {
-        coin.SetActive(false);
-        bool coinvisible = false;
+    // Use this for initialization
+    void Start()
+    {
+        enemy.SetActive(false);
+        bool enemyvisible = false;
         //GameObject coin = (GameObject)Resources.Load("Resources/Coin");
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Vector3 speed = GameObject.Find("Ball").GetComponent<Rigidbody2D>().velocity;
-        print(speed);
-        if (speed.y >= 0)
-            coin.SetActive(true);
+        if (speed.y > 0)
+            enemy.SetActive(true);
     }
 
 }
