4ae936dbbbde95b0679aeb0fe8ce1128c361cd43
oxx1108 <1551193484@qq.com>
2019-02-22 22:50:12 +0800

    添加可以选择level选项
    方便关卡调试，积分等可能会有bug

--- Assets/Scripts/demo2/Loader.cs
+++ Assets/Scripts/demo2/Loader.cs
@@ -6,7 +6,21 @@ using UnityEngine.UI;
 public class Loader : MonoBehaviour {
     //这个脚本用来控制每个场景中唯一只有一个GameManager
     public GameObject gamemanager;
+    public Transform _camera;
+    public Transform _ball;
     // Use this for initialization
+
+    private void SetLevel(int level)
+    {
+        Vector3 target;
+        target = _camera.position;
+        target.y = target.y + 10.24f * level;
+        _camera.position = target;
+        target = _ball.position;
+        target.y = target.y + 10.24f * level;
+        _ball.position = target;
+    } 
+    
     private void Awake()
     {
         if (ToolManager.Instance == null)
@@ -19,6 +33,7 @@ public class Loader : MonoBehaviour {
         GameObject.Find("Height").GetComponent<Text>().text = "Height: 0";
         PlayerPrefs.SetInt("max height", 0);
         PlayerPrefs.SetInt("score", 0);
+        SetLevel(PlayerPrefs.GetInt("level"));
         //ToolManager.Instance.ball = GameObject.FindGameObjectWithTag("Player");
         ToolManager.Instance.IniToolmanager(); 
     }
