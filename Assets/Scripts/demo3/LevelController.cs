bcb40c2a948982701087b554e8a0d956e6bc1ad7
CannonTortoise <ruiw2006@126.com>
2019-02-27 17:16:22 +0800

    新机制

---
+++ Assets/Scripts/demo3/LevelController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+    [System.Serializable]
+    public struct Step
+    {
+        public int lstep;
+        public int mstep;
+        public int rstep;
+    }
+
+    public int totalLevel;
+    private int currentLevel;
+    public Vector2[] resetPos;
+    public Step[] levelStep;
+    private JumpController jc;
+    private GameObject levelPrefab;
+
+
+    void Start () {
+        jc = GameObject.Find("Ball").GetComponent<JumpController>();
+        ChangeLevel(0);
+    }
+
+    public void ChangeLevel(int i) {
+        currentLevel = i;
+        jc.SetSteps(levelStep[currentLevel].lstep, levelStep[currentLevel].mstep, levelStep[currentLevel].rstep);
+        levelPrefab = (GameObject)Resources.Load("Level" + currentLevel);
+    }
+
+    public void NextLevel()
+    {
+        ChangeLevel(currentLevel+1);
+    }
+
+    public void ResetLevel()
+    {
+        string levelname = "Level" + currentLevel + "(Clone)";
+        Destroy(GameObject.Find(levelname));
+        Instantiate(levelPrefab);
+        GameObject ball = GameObject.Find("Ball");
+        ball.transform.position = resetPos[currentLevel];
+        ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        jc.SetSteps(levelStep[currentLevel].lstep, levelStep[currentLevel].mstep, levelStep[currentLevel].rstep);
+    }
+}
