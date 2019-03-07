bc4d33ff6f558e4066052218f0668e4b8a490c1e
CannonTortoise <ruiw2006@126.com>
2019-03-08 00:05:07 +0800

    改动

---
+++ Assets/Scripts/demo3/Resolusion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolusion : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.SetResolution(600, 960, false);
+    }
+}
